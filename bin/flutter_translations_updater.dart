0ae6ec7573a50834a91d59604d6b90e49be70766
Yuriy Trofimov <yuriy.trofimov@lanars.com>
2022-07-08 17:15:52 +0300

    Create first implementation of translation updater

--- bin/flutter_translations_updater.dart
+++ bin/flutter_translations_updater.dart
@@ -1,5 +1,74 @@
-import 'package:flutter_translations_updater/flutter_translations_updater.dart' as flutter_translations_updater;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${flutter_translations_updater.calculate()}!');
+import 'package:args/args.dart';
+import 'package:http/http.dart' as http;
+
+void main(List<String> arguments) async {
+  final parser = ArgParser()
+    ..addFlag(
+      'help',
+      abbr: 'h',
+      negatable: false,
+      help: 'Information about usage tool',
+    )
+    ..addSeparator('=== Parameters ===')
+    ..addFlag(
+      'languages',
+      negatable: false,
+      help: '''
+You need insert links for download translation files with language codes
+Example "en=URL" or "en_CA=URL" "en_FR=URL"
+* It is also necessary to take into account that the main file for transfers must not have a country code.
+''',
+    )
+    ..addFlag(
+      'folder-path',
+      negatable: false,
+      help: '''
+Here you need to substitute the path to save the translation files.
+Example "lib/l10"''',
+    );
+
+  final argResults = parser.parse(arguments);
+
+  if (argResults.wasParsed('help')) {
+    print(parser.usage);
+    exit(0);
+  }
+  final languagesList = <String>[];
+  String? path;
+
+  if (argResults.arguments.first == '--folder-path') {
+    path = argResults.arguments[1];
+    languagesList.addAll(
+      argResults.arguments.getRange(3, argResults.arguments.length),
+    );
+  } else {
+    path = argResults.arguments.last;
+    languagesList.addAll(
+      argResults.arguments.getRange(1, argResults.arguments.length - 2),
+    );
+  }
+  final languagesMap = <String, String>{};
+  for (final element in languagesList) {
+    final index = element.indexOf('=');
+    languagesMap[element.substring(0, index)] = element.substring(
+      index + 1,
+      element.length,
+    );
+  }
+
+  languagesMap.forEach(
+    (key, value) async {
+      final result = await http.get(Uri.parse(value));
+      if (result.statusCode == HttpStatus.ok && path != null) {
+        await File('$path/app_$key.arb').create(recursive: true).then(
+          (file) {
+            file.writeAsString(result.body);
+          },
+        );
+      }
+    },
+  );
+  exit(0);
 }
